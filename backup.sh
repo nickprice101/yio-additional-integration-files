9450fffee041c712834c09cb21eae0ddda754a98
Nick Price <nickprice@corsicanescape.com>
2020-06-01 13:53:18 +0200

    initial upload

--- backup.sh
+++ backup.sh
@@ -3,7 +3,6 @@ cp ~/projects/yio/remote-software/components/media_player/ui/CardSpeakers.qml ~/
 cp ~/projects/yio/remote-software/components/media_player/ui/CardSearch.qml ~/projects/yio/backups/
 cp ~/projects/yio/remote-software/components/media_player/ui/Card.qml ~/projects/yio/backups/
 cp ~/projects/yio/remote-software/basic_ui/ContextMenu.qml ~/projects/yio/backups/
-cp ~/projects/yio/binaries/config.json ~/projects/yio/backups/
 cp ~/projects/yio/integrations.library/src/yio-interface/entities/mediaplayerinterface.h ~/projects/yio/backups/
 cp ~/projects/yio/remote-software/sources/entities/mediaplayer.cpp ~/projects/yio/backups/
 cp ~/projects/yio/remote-software/sources/entities/mediaplayer.h ~/projects/yio/backups/
--- restore.sh
+++ restore.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+cp ~/projects/yio/backups/CardSpeakers.qml ~/projects/yio/remote-software/components/media_player/ui/
+cp ~/projects/yio/backups/CardSearch.qml ~/projects/yio/remote-software/components/media_player/ui/
+cp ~/projects/yio/backups/Card.qml ~/projects/yio/remote-software/components/media_player/ui/
+cp ~/projects/yio/backups/ContextMenu.qml ~/projects/yio/remote-software/basic_ui/
+cp ~/projects/yio/backups/mediaplayerinterface.h ~/projects/yio/integrations.library/src/yio-interface/entities/
+cp ~/projects/yio/backups/mediaplayer.cpp ~/projects/yio/remote-software/sources/entities/
+cp ~/projects/yio/backups/mediaplayer.h ~/projects/yio/remote-software/sources/entities/
